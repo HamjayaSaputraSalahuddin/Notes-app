3f502c7894b5b179a2b2a8d50b943e1f0c156432
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2023-06-11 04:50:26 +0800

    Register plugin at server Hapi

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 // const notesPlugin = require('./notesPlugin');
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require ('./api/notes')
+const NotesService = require('./services/inMemory/NotesServices');
 
 const init = async () => {
+  const notesService = new NotesService(); 
   const server = Hapi.server(
     {
       port: 5000,
@@ -16,12 +18,12 @@ const init = async () => {
   );
 
   
-  // await server.register({
-  //   plugin: notesPlugin,
-  //   options: { notes: [] },
-  // });
-
-  server.route(routes);
+  await server.register({
+    plugin: notesPlugin,
+    options: { 
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
