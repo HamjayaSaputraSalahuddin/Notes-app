ca1383f7659ad0cc9b8ef62708cc22892ae11e51
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2023-06-10 08:58:51 +0800

    restructure folder and install some package

--- src/server.js
+++ src/server.js
@@ -1,28 +1,30 @@
-const notesPlugin = require('./notesPlugin');
+// const notesPlugin = require('./notesPlugin');
 const Hapi = require('@hapi/hapi');
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const init = async () => {
-  const server = Hapi.server();
-  //   {
-  //   port: 5000,
-  //   host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-  //   routes: {
-  //     cors: {
-  //       origin: ['*'],
-  //     },
-  //   },
-  // }
-  
-  await server.register({
-    plugin: notesPlugin,
-    options: { notes: [] },
-  });
+  const server = Hapi.server(
+    {
+      port: 5000,
+      host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
+      },
+    }
+  );
 
-  // server.route(routes);
+  
+  // await server.register({
+  //   plugin: notesPlugin,
+  //   options: { notes: [] },
+  // });
+
+  server.route(routes);
 
   await server.start();
-  // console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
